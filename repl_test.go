765e2a91a54614148775a51f56fc60f8f4866edd
dta0one <github@davidtao.net>
2026-09-08 19:18:13 -0700

    created test files and initial tests per TDD

---
+++ repl_test.go
@@ -0,0 +1,37 @@
+package main
+
+import "testing"
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "Charmander Bulbasaur PIKACHU",
+			expected: []string{"charmander", "bulbasaur", "pikachu"},
+		},
+		// You can add more test cases here!
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		
+		if len(actual) != len(c.expected) {
+			t.Errorf("mismatched lengths for input %q: got %d, expected %d", c.input, len(actual), len(c.expected))
+			continue
+		}
+
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+			if word != expectedWord {
+				t.Errorf("at index %d for input %q: got %q, expected %q", i, c.input, word, expectedWord)
+			}
+		}
+	}
+}

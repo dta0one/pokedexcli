8e7644c6ba5316917b9580d812749ba253059c94
dta0one <github@davidtao.net>
2026-09-11 08:45:56 -0700

    1.4 first REPL pattern

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		// Wait for user input
+		if !scanner.Scan() {
+			break
+		}
+
+		input := scanner.Text()
+
+		// Clean input: lowercase and split into words
+		words := cleanInput(input)
+		if len(words) == 0 {
+			continue
+		}
+
+		firstWord := words[0]
+		fmt.Printf("Your command was: %s\n", firstWord)
+	}
 }

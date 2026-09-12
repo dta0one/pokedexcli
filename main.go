c1bb634d6c579601cdee054a1ab4b45224aa73fe
dta0one <github@davidtao.net>
2026-09-12 08:32:31 -0700

    help and exit lesson

--- main.go
+++ main.go
@@ -1,31 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("Pokedex > ")
-
-		// Wait for user input
-		if !scanner.Scan() {
-			break
-		}
-
-		input := scanner.Text()
-
-		// Clean input: lowercase and split into words
-		words := cleanInput(input)
-		if len(words) == 0 {
-			continue
-		}
-
-		firstWord := words[0]
-		fmt.Printf("Your command was: %s\n", firstWord)
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,9 +1,83 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
 
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+	commands := getCommands()
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		if !scanner.Scan() {
+			break
+		}
+
+		words := cleanInput(scanner.Text())
+		if len(words) == 0 {
+			continue
+		}
+
+		commandName := words[0]
+
+		cmd, exists := commands[commandName]
+		if !exists {
+			fmt.Println("Unknown command")
+			continue
+		}
+
+		err := cmd.callback()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+}
+
 func cleanInput(text string) []string {
 	return strings.Fields(strings.ToLower(text))
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println()
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+
+	for _, cmd := range getCommands() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	fmt.Println()
+	return nil
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 }

88c9b6a645bb4e01c460a789a7f8ef0dcfe7165c
dta0one <github@davidtao.net>
2026-09-16 08:39:08 -0700

    2.6 Inspect

--- pokeapi.go
+++ pokeapi.go
@@ -108,6 +108,19 @@ func fetchLocationArea(cfg *config, areaName string) (LocationAreaResponse, erro
 type Pokemon struct {
 	Name           string `json:"name"`
 	BaseExperience int    `json:"base_experience"`
+	Height         int    `json:"height"`
+	Weight         int    `json:"weight"`
+	Stats          []struct {
+		BaseStat int `json:"base_stat"`
+		Stat     struct {
+			Name string `json:"name"`
+		} `json:"stat"`
+	} `json:"stats"`
+	Types []struct {
+		Type struct {
+			Name string `json:"name"`
+		} `json:"type"`
+	} `json:"types"`
 }
 
 func fetchPokemon(cfg *config, pokemonName string) (Pokemon, error) {
--- repl.go
+++ repl.go
@@ -151,6 +151,11 @@ func getCommands() map[string]cliCommand {
 			description: "Attempt to catch a pokemon",
 			callback:    commandCatch,
 		},
+		"inspect": {
+			name:        "inspect",
+			description: "View details about a caught Pokemon",
+			callback:    commandInspect,
+		},
 	}
 }
 
@@ -199,6 +204,38 @@ func commandCatch(cfg *config, args []string) error {
 		cfg.pokedex[pokemon.Name] = pokemon
 	} else {
 		fmt.Printf("%s escaped!\n", pokemon.Name)
+	}
+
+	return nil
+}
+
+func commandInspect(cfg *config, args []string) error {
+	if len(args) != 1 {
+		return fmt.Errorf("inspect requires exactly one pokemon name")
+	}
+
+	pokemonName := args[0]
+
+	// Check if the pokemon exists in the user's pokedex map
+	pokemon, ok := cfg.pokedex[pokemonName]
+	if !ok {
+		fmt.Println("you have not caught that pokemon")
+		return nil
+	}
+
+	// If caught, print out the details exactly as requested
+	fmt.Printf("Name: %s\n", pokemon.Name)
+	fmt.Printf("Height: %d\n", pokemon.Height)
+	fmt.Printf("Weight: %d\n", pokemon.Weight)
+
+	fmt.Println("Stats:")
+	for _, stat := range pokemon.Stats {
+		fmt.Printf("  -%s: %d\n", stat.Stat.Name, stat.BaseStat)
+	}
+
+	fmt.Println("Types:")
+	for _, typeInfo := range pokemon.Types {
+		fmt.Printf("  - %s\n", typeInfo.Type.Name)
 	}
 
 	return nil
